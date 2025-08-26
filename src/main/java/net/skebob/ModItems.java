b70d4b9584c87219aa44b71dc61a670380149d06
Dmitry <andrewgamer63@gmail.com>
2025-08-26 03:04:23 +0300

    Initial commit

---
+++ src/main/java/net/skebob/ModItems.java
@@ -0,0 +1,36 @@
+package net.skebob;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+import static net.minecraft.item.Items.register;
+
+public class ModItems {
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Skebob.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+    public static final Item SKEBOB = register("Skebob", Item::new, new Item.Settings());
+
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS)
+                .register((itemGroup) -> itemGroup.add(ModItems.SKEBOB));
+    }
+}
